35d74d3cf7e0e091885fb1acb22e0b706408566a
Anna D Derbakova <hiopa16@gmail.com>
2020-06-03 14:39:08 -0400

    Adding 5 second sleep after login

--- incremental_scraper.py
+++ incremental_scraper.py
@@ -145,6 +145,7 @@ def extract(browser, page, usage, scrape_comment=False):
     browser.find_element_by_name("email").send_keys(email)
     browser.find_element_by_name("pass").send_keys(password)
     browser.find_element_by_id('loginbutton').click()
+    time.sleep(5)
 
     # Navigate to the Posts page
     browser.get("http://facebook.com/" + page + "/posts")
